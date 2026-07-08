12204f8e2b1a744f70f0015de03072826c904cc6
Mukhammad Akbar <akbaraliy111@example.com>
2026-07-08 13:58:58 +0500

    homework 6

--- month-2/6-lesson/src/main.js
+++ month-2/6-lesson/src/main.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { config } from 'dotenv';
 config();
+
 import { connectDB } from './config/db.js';
+import router from './router/user.route.js';
 
 const app = express();
 const PORT = Number(process.env.PORT_API);
@@ -9,5 +11,7 @@ const PORT = Number(process.env.PORT_API);
 app.use(express.json());
 
 await connectDB();
+
+app.use('/', router)
 
 app.listen(PORT, () => console.log(`Server running on port`, PORT));
